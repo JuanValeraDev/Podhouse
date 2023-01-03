bba9df00d090f5e179ce44817f4f18da32986c92
JuanValeraDAM <3690820@alu.murciaeduca.es>
2023-01-03 12:17:21 +0100

    commit 03/01/2023

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
-const elemento = document.querySelector('.elemento-flotante');
+/*
 
-let isDragging = false;
-let currentX;
-let currentY;
-let initialX;
-let initialY;
-let xOffset = 0;
-let yOffset = 0;
+TODO: Al pinchar en una fila de la tabla ya seleccionada, que se deseleccione
+*/
 
-elemento.addEventListener("mousedown", dragStart);
-elemento.addEventListener("mouseup", dragEnd);
-elemento.addEventListener("mousemove", drag);
 
-function dragStart(e) {
-  initialX = e.clientX - xOffset;
-  initialY = e.clientY - yOffset;
+const botonCerrar = document.querySelector('.boton-cerrar-reproductor');
+const reproductorCompleto = document.getElementById('reproductor-completo');
+const reproductor = document.querySelector('.reproductor');
 
-  if (e.target === elemento) {
-    isDragging = true;
-  }
+
+botonCerrar.addEventListener('click', () => {
+  reproductor.pause();
+  reproductorCompleto.style.display = 'none';
+});
+
+
+const botonesPlay = document.querySelectorAll('.play-button-table');
+for (const boton of botonesPlay) {
+  boton.addEventListener('click', () => {
+    reproductorCompleto.style.display = 'flex';
+
+  });
+}
+document.getElementById('botonScroll').addEventListener('click', function() {
+  document.getElementById('tabla').scrollIntoView({behavior:'smooth'});
+});
+
+
+const tabla = document.getElementById("tabla");
+let selectedRow;
+
+for (let i = 0; i < tabla.rows.length; i++) {
+  tabla.rows[i].addEventListener("click", function() {
+    // Si ya hay una fila seleccionada, restaura su color original
+    if (selectedRow) {
+      selectedRow.style.backgroundColor = "";
+      selectedRow.style.color="";
+    }
+    // Establece el color de fondo de la fila seleccionada
+    this.style.backgroundColor = "rgb(37, 23, 73)";
+    this.style.color="rgb(255, 251, 235)";
+    // Almacena el id de la fila seleccionada
+    selectedRow = this;
+  });
 }
 
-function dragEnd(e) {
-  initialX = currentX;
-  initialY = currentY;
 
-  isDragging = false;
-}
-
-function drag(e) {
-  if (isDragging) {
-    e.preventDefault();
-    currentX = e.clientX - initialX;
-    currentY = e.clientY - initialY;
-
-    xOffset = currentX;
-    yOffset = currentY;
-
-    setTranslate(currentX, currentY, elemento);
-  }
-}
-
-function setTranslate(xPos, yPos, el) {
-  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-}
