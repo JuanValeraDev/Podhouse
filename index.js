eab5d4b3eb81fe716960db27df0aac7bfa7f62f0
JuanValeraDAM <3690820@alu.murciaeduca.es>
2023-01-07 19:47:34 +0100

    Diseño de la web bastante completito. 07/01/2023

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /*
+TODO: Que al hacer hover sobre una fila de la tabla aparezca un dialogo con la imagen y una descripción del programa.
+Que solo se pueda abrir el modal de editar teniendo seleccionada una fila.
+El modal de edición debe abrirse con los datos de esa fila.
 
-TODO: Al pinchar en una fila de la tabla ya seleccionada, que se deseleccione
 */
 
 
@@ -8,11 +10,13 @@ const botonCerrar = document.querySelector('.boton-cerrar-reproductor');
 const reproductorCompleto = document.getElementById('reproductor-completo');
 const reproductor = document.querySelector('.reproductor');
 
-
 botonCerrar.addEventListener('click', () => {
   reproductor.pause();
   reproductorCompleto.style.display = 'none';
+  console.log(`Display del reproductor: ${reproductorCompleto.style.display}`);
+
 });
+
 
 
 const botonesPlay = document.querySelectorAll('.play-button-table');
@@ -26,6 +30,8 @@ document.getElementById('botonScroll').addEventListener('click', function () {
   document.getElementById('tabla').scrollIntoView({ behavior: 'smooth' });
 });
 
+
+
 const tabla = document.getElementById('tabla');
 let selectedRow;
 
@@ -37,7 +43,7 @@ tabla.addEventListener('click', (event) => {
       row.style.color = '#251749'
       selectedRow = null;
     } else {
-      row.style.backgroundColor = '#251749';
+      row.style.backgroundColor = '#263159';
       row.style.color = '#FFFBEB';
       if (selectedRow) {
         selectedRow.style.backgroundColor = '';
@@ -49,17 +55,39 @@ tabla.addEventListener('click', (event) => {
 });
 
 
-
-
-
-
-// ---- Modal -----
 const modal = new bootstrap.Modal(document.getElementById('informacion-podcast'));
-
 
 document.getElementById('boton-editar').addEventListener('click', abrirModal);
 document.getElementById('boton-insertar').addEventListener('click', abrirModal);
+const inputSubida = document.getElementById('input-subida');
+
 function abrirModal(evt) {
-  modal.show();
+  if (evt.target.classList.contains('boton-insertar')) {
+    inputSubida.classList.remove("d-none");
+    modal.show();
+  } else {
+    inputSubida.classList.add("d-none");
+    modal.show();
+  }
+
 }
-  
+
+
+
+const rows = document.querySelectorAll('.table tbody tr');
+
+rows.forEach(row => {
+  const botonReproducir = row.querySelector('td p .btn');
+  botonReproducir.addEventListener('click', () => {
+
+    const titulo = row.querySelector('.titulo-fila').textContent;
+    const imagen = row.querySelector('.imagen-dentro-de-tabla').src;
+    const audio = row.querySelector('.audio-dentro-de-tabla').src;
+
+    document.querySelector('.imagen-reproductor').src = imagen;
+    document.querySelector('.titulo-reproductor').textContent = titulo;
+    document.querySelector('.reproductor').src = audio;
+  });
+});
+
+
